51282e1b529e3c927c4fe79a15bdaa718a8731c9
Arnaud Lheureux <arnaudlh@hotmail.com>
2019-11-19 18:46:40 +0900

    VPN gateway commented

--- landingzone_vdc_demo/blueprint_networking_shared_transit/output.tf
+++ landingzone_vdc_demo/blueprint_networking_shared_transit/output.tf
@@ -2,9 +2,9 @@ output "transit_networking_transit_vnet" {
     value       = module.networking_transit_vnet.vnet
 }
 
-output "transit_virtual_network_gateway" {
-    value       = module.vpn_gateway.object
-}
+# output "transit_virtual_network_gateway" {
+#     value       = module.vpn_gateway.object
+# }
 
 output "transit_networking_transit_public_ip" {
     value       = module.networking_transit_public_ip.ip_address
