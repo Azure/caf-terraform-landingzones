905913e1543c1e8080e458f3213291985c15e555
Nicolas Yuen <nicolas.yuen@gmail.com>
2020-01-08 11:30:33 +0800

    fixing variable expension -> prefixing with variables. as per the yank doc

--- test/landing_zone_caf_foundationv2/sandpit_autotfvars.sh
+++ test/landing_zone_caf_foundationv2/sandpit_autotfvars.sh
@@ -1,2 +1,4 @@
 resource_groups_hub_ids = terraform output -json resource_group_hub_ids | jq 
 log_analytics_worskpace_name = terraform output -json  log_analytics_workspace
+
+#todo -> compare the terraform output with the resource graph or use static 
