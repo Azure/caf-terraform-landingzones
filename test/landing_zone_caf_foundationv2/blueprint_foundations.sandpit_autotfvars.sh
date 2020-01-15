4c844f1c2904e6156dedc2934390126fcb0974d6
Nicolas Yuen <nicolas.yuen@gmail.com>
2020-01-15 07:09:02 +0000

    first unit test sample

---
+++ test/landing_zone_caf_foundationv2/blueprint_foundations.sandpit_autotfvars.sh
@@ -0,0 +1,29 @@
+resource_groups_hub_ids = terraform output -json resource_group_hub_ids | jq 
+log_analytics_worskpace_name = terraform output -json  log_analytics_workspace
+
+#todo -> compare the terraform output with the resource graph or use static 
+
+#validate that the resource group defined have been created for the accounting module 
+echo "validation accounting resource group cont"
+blueprint_foundations_accounting_rg_count = az graph query -q "ResourceContainers | where type =~ 'microsoft.resources/subscriptions/resourcegroups' | where tags['blueprint'] =~ 'blueprint_foundations_accounting'" | jq 'length'
+
+echo "testing HUB-CORE-SEC"
+hub_core_sec_storage_account_count =  az graph query -q "Resources | where resourceGroup contains 'HUB-CORE-SEC' and type =~ 'microsoft.storage/storageaccounts' | count" | jq .[].C
+
+hub_core_sec_storage_account_tag_terraform = az graph query -q "Resources | where resourceGroup contains 'HUB-CORE-SEC' and type =~ 'microsoft.storage/storageaccounts' " | jq '.[0] | .tags["deploymentType"]'
+hub_core_sec_storage_account_primary_location = az graph query -q "Resources | where resourceGroup contains 'HUB-CORE-SEC' and type =~ 'microsoft.storage/storageaccounts' " | jq '.[0] | .properties.primaryLocation'
+
+hub_core_sec_event_hub_count = az graph query -q "Resources | where resourceGroup contains 'HUB-CORE-SEC' and type =~ 'Microsoft.EventHub/namespaces' | count" | jq .[].Count
+hub_core_sec_event_hub_location = az graph query -q "Resources | where resourceGroup contains 'HUB-CORE-SEC' and type =~ 'Microsoft.EventHub/namespaces'" | jq .[].location
+
+echo "testing HUB-OPERATIONS"
+hub_operations_rg_count = az graph query -q "ResourceContainers | where type =~ 'microsoft.resources/subscriptions/resourcegroups' | where name contains '-hub-operations'" | jq 'length'
+hub_operations_resource_count= az graph query -q "Resources | where resourceGroup contains '-hub-operations'" | jq 'length'
+hub_operations_monitor_logs_count = az graph query -q "Resources | where resourceGroup contains '-hub-operations' and type =~ 'Microsoft.OperationalInsights/workspaces'" | jq 'length'
+hub_operations_monitor_logs_name = az graph query -q "Resources | where resourceGroup contains '-hub-operations' and type =~ 'Microsoft.OperationalInsights/workspaces'" | jq '.[0].name | contains("lalogs")'
+hub_operations_storage_account_count =  az graph query -q "Resources | where resourceGroup contains '-hub-operations' and type =~ 'microsoft.storage/storageaccounts' | count" | jq .[].Count
+hub_operations_storage_account_primary_location = az graph query -q "Resources | where resourceGroup contains '-hub-operations' and type =~ 'microsoft.storage/storageaccounts' " | jq '.[0] | .properties.primaryLocation'
+
+echo "testing GOVERNANCE policies"
+policy_vm_no_managed_disks_exists = az policy assignment list | jq '.[] | select(.name | contains("vm_no_managed_disks")) | .name'
+
--- test/landing_zone_caf_foundationv2/sandpit_autotfvars.sh
+++
@@ -1,4 +0,0 @@
-resource_groups_hub_ids = terraform output -json resource_group_hub_ids | jq 
-log_analytics_worskpace_name = terraform output -json  log_analytics_workspace
-
-#todo -> compare the terraform output with the resource graph or use static 
