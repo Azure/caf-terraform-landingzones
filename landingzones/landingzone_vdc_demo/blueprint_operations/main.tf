c9695f8af75a5723d2a08ccf84008bd3e1bfb671
Arnaud Lheureux <arnaudlh@hotmail.com>
2020-02-13 00:29:28 +0000

    azurerm 1.44

--- landingzones/landingzone_vdc_demo/blueprint_operations/main.tf
+++ landingzones/landingzone_vdc_demo/blueprint_operations/main.tf
@@ -4,7 +4,7 @@ terraform {
 }
 
 provider "azurerm" {
-  version = "<= 1.42"
+  version = "<= 1.44"
 }
 
 data "azurerm_subscription" "current" {
--- landingzones/landingzone_vdc_demo/main.tf
+++ landingzones/landingzone_vdc_demo/main.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  version = "<=1.42"
+  version = "<=1.44"
 }
 
 terraform {
