29637611d3bd0d599fe386fad7b61643a5ee5db1
nunocenteno <nunocenteno@users.noreply.github.com>
2020-04-08 23:13:55 +0000

    Avoid creating unecessary policy definitions.

--- landingzones/landingzone_caf_foundations/blueprint_foundations_governance/policies/custom/pip_on_nic.tf
+++ landingzones/landingzone_caf_foundations/blueprint_foundations_governance/policies/custom/pip_on_nic.tf
@@ -1,5 +1,6 @@
 
 resource "azurerm_policy_definition" "deny_publicips_on_nics" {
+  count		   = var.policies_matrix.no_public_ip_spoke ? 1 : 0
   name         = "pol-deny-publicips-on-nics"
   policy_type  = "Custom"
   mode         = "Indexed"
@@ -31,7 +32,7 @@ resource "azurerm_policy_assignment" "publicIP-deny-on-nics" {
   count               = var.policies_matrix.no_public_ip_spoke ? 1 : 0
   name                = "deny-publicip-on-nics"
   scope                = var.scope
-  policy_definition_id = azurerm_policy_definition.deny_publicips_on_nics.id
+  policy_definition_id = azurerm_policy_definition.deny_publicips_on_nics[0].id
   description          = "Policy Assignment for deny public IP on NICs"
   display_name         = "TF Deny public IP assignment on NICs"
 
