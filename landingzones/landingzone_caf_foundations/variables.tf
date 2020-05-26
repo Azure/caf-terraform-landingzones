0c7f211602036a0defc20e1eada34dfef73d1ef7
Arnaud Lheureux <arnaudlh@hotmail.com>
2020-05-26 10:27:52 +0000

    azurerm 2.11 and updated workflows

--- landingzones/landingzone_caf_foundations/variables.tf
+++ landingzones/landingzone_caf_foundations/variables.tf
@@ -1,14 +1,26 @@
-# Map of the remote data state
+# Map of the remote data state filled by the rover at runtime
 variable "lowerlevel_storage_account_name" {}
 variable "lowerlevel_container_name" {}
 variable "lowerlevel_key" {}                  # Keeping the key for the lower level0 access
 variable "lowerlevel_resource_group_name" {}
+
+
+# Set of variables for the CAF foundations landing zone
 variable "tags" {
     type = map
     default = {}
 }
 
-variable "global_settings" {}
-variable "accounting_settings" {}
-variable "security_settings" {}
-variable "governance_settings" {}
+variable "global_settings" {
+    description = "(Required) object describing global settings for landing zone configuration (region, naming convention etc.)"
+}
+
+variable "accounting_settings" {
+    description = "(Required) object describing accounting settings for landing zone configuration (azure monitor log analytics, storage accounts, etc.)"
+}
+variable "security_settings" {
+    description = "(Required) object describing security settings for landing zone configuration (azure security center standard, azure sentinel enablement.)"
+}
+variable "governance_settings" {
+    description = "(Required) object describing governance settings for landing zone configuration (azure policies and azure management groups)"
+}
