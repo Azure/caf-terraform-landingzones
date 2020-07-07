c3076970b873c61a8b677d8a612e2e502a162ea5
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2020-07-07 03:27:20 +0000

    Adding iteration for hubs and spokes

--- landingzones/landingzone_hub_mesh/output.tf
+++ landingzones/landingzone_hub_mesh/output.tf
@@ -4,17 +4,12 @@ output "virtual_wan" {
   value       = azurerm_virtual_wan.vwan
 }
 
-output "hub1" {
+output "hubs" {
   description = "Output the full object for Virtual Hub 1"
   sensitive   = false # to hide content from logs
-  value       = module.virtual_hub_region1
+  value       = module.virtual_hub.*
 }
 
-output "hub2" {
-  description = "Output the full object for Virtual Hub 2"
-  sensitive   = false # to hide content from logs
-  value       = module.virtual_hub_region2
-}
 
 ## re-exporting level1 settings (caf_foundations) for level 3 consumption
 output "prefix" {
