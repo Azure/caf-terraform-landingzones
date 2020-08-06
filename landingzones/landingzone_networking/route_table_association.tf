a010a7bd26a17c3c32acf0a881d99eba635193aa
Arnaud Lheureux <arnaudlh@hotmail.com>
2020-08-06 10:10:27 +0000

    Adding dependency

--- landingzones/landingzone_networking/route_table_association.tf
+++ landingzones/landingzone_networking/route_table_association.tf
@@ -1,4 +1,6 @@
 resource "azurerm_subnet_route_table_association" "route_subnet" {
+  depends_on = [azurerm_route_table.route_table]
+  
   for_each = var.route_tables
 
   subnet_id      = module.vnets[each.value.vnet_key].vnet_subnets[each.value.subnet_key]
