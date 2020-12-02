94fc56fb4888f5555b8685bd6e1aabc1cd352b32
lolorol <LaurentLesle@users.noreply.github.com>
2020-12-02 09:05:29 +0000

    Update for remote diagnostics objects

--- landingzones/caf_foundations/locals.remote_tfstates.tf
+++ landingzones/caf_foundations/locals.remote_tfstates.tf
@@ -38,6 +38,12 @@ locals {
 
   remote = {
     diagnostics = {
+      # Get the diagnostics settings of services to create
+      diagnostic_event_hub_namespaces     = var.diagnostic_event_hub_namespaces
+      diagnostic_log_analytics            = var.diagnostic_log_analytics
+      diagnostic_storage_accounts         = var.diagnostic_storage_accounts
+      
+      # Combine the diagnostics definitions
       diagnostics_definition   = merge(data.terraform_remote_state.remote[var.landingzone.global_settings_key].outputs.diagnostics.diagnostics_definition, var.diagnostics_definition)
       diagnostics_destinations = {
         event_hub_namespaces = merge(
@@ -53,10 +59,12 @@ locals {
           try(var.diagnostics_destinations.storage, {})
         )
       }
+      # Get the remote existing diagnostics objects
       storage_accounts         = data.terraform_remote_state.remote[var.landingzone.global_settings_key].outputs.diagnostics.storage_accounts
       log_analytics            = data.terraform_remote_state.remote[var.landingzone.global_settings_key].outputs.diagnostics.log_analytics
       event_hub_namespaces     = data.terraform_remote_state.remote[var.landingzone.global_settings_key].outputs.diagnostics.event_hub_namespaces
     }
+    
     managed_identities = {
       for key, value in try(var.landingzone.tfstates, {}) : key => merge(try(data.terraform_remote_state.remote[key].outputs.managed_identities[key], {}))
     }
