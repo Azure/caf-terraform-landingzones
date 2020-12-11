776892cde0db212c7cb570e172e567368e0ef995
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2020-12-11 19:44:00 +0800

    Update dynamic_secrets.tf

--- landingzones/caf_launchpad/dynamic_secrets.tf
+++ landingzones/caf_launchpad/dynamic_secrets.tf
@@ -1,7 +1,7 @@
 
 module dynamic_keyvault_secrets {
   source  = "aztfmod/caf/azurerm//modules/security/dynamic_keyvault_secrets"
-  version = "~> 0.4"
+  version = "0.4.20"
   
   for_each = try(var.dynamic_keyvault_secrets, {})
 
