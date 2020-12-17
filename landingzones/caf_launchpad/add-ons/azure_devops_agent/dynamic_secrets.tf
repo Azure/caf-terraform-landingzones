7a8076d0e65e860aaf4bd572e63dccf1c8663c00
Arnaud Lheureux <arnaud@msft.com>
2020-12-17 15:00:19 +0000

    Update 2012.1.0

--- landingzones/caf_launchpad/add-ons/azure_devops_agent/dynamic_secrets.tf
+++ landingzones/caf_launchpad/add-ons/azure_devops_agent/dynamic_secrets.tf
@@ -1,9 +1,11 @@
 
 module dynamic_keyvault_secrets {
-  source   = "./dynamic_keyvault_secrets"
+  source  = "aztfmod/caf/azurerm//modules/security/dynamic_keyvault_secrets"
+  version = "~>4.21"
+  
   for_each = try(var.dynamic_keyvault_secrets, {})
 
   settings    = each.value
-  keyvault_id = module.caf.keyvaults[each.key].id
+  keyvault    = module.caf.keyvaults[each.key]
   objects     = module.caf
 }
--- landingzones/caf_launchpad/dynamic_secrets.tf
+++ landingzones/caf_launchpad/dynamic_secrets.tf
@@ -1,11 +1,11 @@
 
 module dynamic_keyvault_secrets {
   source  = "aztfmod/caf/azurerm//modules/security/dynamic_keyvault_secrets"
-  version = "0.4.20"
+  version = "~>4.21"
   
   for_each = try(var.dynamic_keyvault_secrets, {})
 
   settings    = each.value
-  keyvault_id = module.launchpad.keyvaults[each.key].id
+  keyvault    = module.launchpad.keyvaults[each.key]
   objects     = module.launchpad
 }
