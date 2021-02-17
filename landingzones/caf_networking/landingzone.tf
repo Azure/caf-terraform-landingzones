7bda406d162b85893095cc589d218163866dda55
Arnaud Lheureux <arnaud@microsoft.com>
2021-02-17 06:52:06 +0000

    Adding dns fields

--- landingzones/caf_networking/landingzone.tf
+++ landingzones/caf_networking/landingzone.tf
@@ -14,45 +14,47 @@ module "networking" {
   keyvaults                = var.keyvaults
   keyvault_access_policies = var.keyvault_access_policies
   networking = {
-    application_gateways                                    = var.application_gateways
     application_gateway_applications                        = var.application_gateway_applications
-    vnets                                                   = var.vnets
-    vnet_peerings                                           = var.vnet_peerings
-    vhub_peerings                                           = var.vhub_peerings
-    network_security_group_definition                       = var.network_security_group_definition
-    azurerm_firewall_network_rule_collection_definition     = var.azurerm_firewall_network_rule_collection_definition
+    application_gateways                                    = var.application_gateways
     azurerm_firewall_application_rule_collection_definition = var.azurerm_firewall_application_rule_collection_definition
     azurerm_firewall_nat_rule_collection_definition         = var.azurerm_firewall_nat_rule_collection_definition
+    azurerm_firewall_network_rule_collection_definition     = var.azurerm_firewall_network_rule_collection_definition
     azurerm_firewalls                                       = var.azurerm_firewalls
-    express_route_circuits                                  = var.express_route_circuits
-    express_route_circuit_authorizations                    = var.express_route_circuit_authorizations
-    public_ip_addresses                                     = var.public_ip_addresses
-    route_tables                                            = var.route_tables
     azurerm_routes                                          = var.azurerm_routes
-    virtual_wans                                            = var.virtual_wans
     ddos_services                                           = var.ddos_services
+    dns_zone_records                                        = var.dns_zone_records
+    dns_zones                                               = var.dns_zones
+    express_route_circuit_authorizations                    = var.express_route_circuit_authorizations
+    express_route_circuits                                  = var.express_route_circuits
+    network_security_group_definition                       = var.network_security_group_definition
     private_dns                                             = var.private_dns
     private_endpoints                                       = var.private_endpoints
+    public_ip_addresses                                     = var.public_ip_addresses
+    route_tables                                            = var.route_tables
+    vhub_peerings                                           = var.vhub_peerings
+    virtual_wans                                            = var.virtual_wans
+    vnet_peerings                                           = var.vnet_peerings
+    vnets                                                   = var.vnets
   }
   compute = {
-    virtual_machines           = var.virtual_machines
-    bastion_hosts              = var.bastion_hosts
     azure_container_registries = var.azure_container_registries
+    bastion_hosts              = var.bastion_hosts
+    virtual_machines           = var.virtual_machines
   }
   storage_accounts   = var.storage_accounts
   managed_identities = var.managed_identities
 
   remote_objects = {
-    application_gateways             = local.remote.application_gateways
     application_gateway_applications = local.remote.application_gateway_applications
+    application_gateways             = local.remote.application_gateways
     azuread_groups                   = local.remote.azuread_groups
     azurerm_firewalls                = local.remote.azurerm_firewalls
     keyvaults                        = local.remote.keyvaults
     managed_identities               = local.remote.managed_identities
     private_dns                      = local.remote.private_dns
     public_ip_addresses              = local.remote.public_ip_addresses
-    vnets                            = local.remote.vnets
     virtual_wans                     = local.remote.virtual_wans
+    vnets                            = local.remote.vnets
   }
 
 }
