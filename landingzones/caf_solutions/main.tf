da3b00f3201f4819c09a7fa5f6fb74ae19939da4
Jor Seng <v-jorsengtan@microsoft.com>
2021-04-07 08:37:21 +0000

    update azurerm constraint

--- landingzones/caf_solutions/main.tf
+++ landingzones/caf_solutions/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~> 2.52"
+      version = "~> 2.52.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
