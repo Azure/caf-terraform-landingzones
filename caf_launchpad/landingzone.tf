b84d781d55d6281ce97cc58b35a99cab14d97613
lolorol <LaurentLesle@users.noreply.github.com>
2021-04-20 22:46:38 +0000

    Update with registry version 5.3.2

--- caf_launchpad/landingzone.tf
+++ caf_launchpad/landingzone.tf
@@ -1,8 +1,8 @@
 module "launchpad" {
-  # source  = "aztfmod/caf/azurerm"
-  # version = "~>5.3.0"
+  source  = "aztfmod/caf/azurerm"
+  version = "~>5.3.2"
 
-  source = "git::https://github.com/aztfmod/terraform-azurerm-caf.git?ref=master"
+  # source = "git::https://github.com/aztfmod/terraform-azurerm-caf.git?ref=master"
 
 
   azuread_api_permissions               = var.azuread_api_permissions
