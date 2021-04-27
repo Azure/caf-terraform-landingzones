2d06e40918c845753d085eb3a1d89694d88d2277
lolorol <LaurentLesle@users.noreply.github.com>
2021-04-27 10:29:39 +0000

    Initial commit

---
+++ caf_solution/add-ons/azure_devops_v1/azdo_agent_pools.tf
@@ -0,0 +1,39 @@
+
+locals {
+  organization_agent_pools = try(var.organization_agent_pools, {})
+  project_agent_pools      = try(var.project_agent_pools, {})
+}
+
+
+
+## Agent pools
+## Those pools are created in the organization, not the project
+resource "azuredevops_agent_pool" "pool" {
+  for_each = var.organization_agent_pools
+
+  name           = each.value.name
+  auto_provision = try(each.value.auto_provision, false)
+  pool_type      = try(each.value.pool_type, null)
+}
+
+#
+# add the agent pools into the project
+#
+
+
+module "project_agent_pools" {
+  source   = "./agent_pools"
+  for_each = var.project_agent_pools
+
+  azuredevops_agent_pools = azuredevops_agent_pool.pool
+  project_id              = data.azuredevops_project.project[each.key].id
+  settings                = each.value
+
+  depends_on = [
+    azuredevops_agent_pool.pool,
+    azuredevops_project.project
+  ]
+}
+
+
+
---
+++ caf_solution/add-ons/azure_devops_v1/output.tf
@@ -0,0 +1,8 @@
+# output "keyvaults" {
+#   value = tomap(
+#     {    
+#       (var.landingzone.key) = module.caf.keyvaults
+#     }
+#   )
+#   sensitive = true
+# }
