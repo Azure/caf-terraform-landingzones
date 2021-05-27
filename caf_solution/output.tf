2665f3e2f0090594123b6a754a14946bda65120b
Arnaud Lheureux <arnaud@microsoft.com>
2021-05-27 13:17:08 +0000

    Output updates

--- caf_solution/output.tf
+++ caf_solution/output.tf
@@ -10,6 +10,16 @@ output "objects" {
   sensitive = true
 }
 
+output "global_settings" {
+  value = local.global_settings
+  sensitive = true
+}
+
+output "diagnostics" {
+  value = local.diagnostics
+  sensitive = true
+}
+
 output "tfstates" {
   value     = local.tfstates
   sensitive = true
