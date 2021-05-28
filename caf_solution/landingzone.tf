aec4fdf25a079fda479bce8153a446f133cdb98b
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2021-05-28 11:19:44 +0800

    Update landingzone.tf

--- caf_solution/landingzone.tf
+++ caf_solution/landingzone.tf
@@ -2,7 +2,7 @@ module "solution" {
   # source  = "aztfmod/caf/azurerm"
   # version = "~>5.3.2"
 
-  source = "git::https://github.com/aztfmod/terraform-azurerm-caf.git?ref=patch_firewall_policy"
+  source = "git::https://github.com/aztfmod/terraform-azurerm-caf.git?ref=master"
 
   # source = "../../aztfmod"
 
