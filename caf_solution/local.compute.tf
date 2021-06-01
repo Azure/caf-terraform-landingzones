315f4359cc8302705c70f0b344a8d85bb636fb9e
Jor Seng <jorseng@gmail.com>
2021-06-01 15:46:18 +0000

    Add vmss to landingzone

--- caf_solution/local.compute.tf
+++ caf_solution/local.compute.tf
@@ -9,6 +9,7 @@ locals {
       container_groups           = var.container_groups
       proximity_placement_groups = var.proximity_placement_groups
       virtual_machines           = var.virtual_machines
+      virtual_machine_scale_sets = var.virtual_machine_scale_sets
       wvd_application_groups     = var.wvd_application_groups
       wvd_host_pools             = var.wvd_host_pools
       wvd_session_hosts          = var.wvd_session_hosts
