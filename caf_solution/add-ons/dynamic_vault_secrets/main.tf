b9588dc7b11cc90b82f1b3f5e8544f1791fef138
Papun Senapati <papun.senapati@servian.com>
2021-06-09 00:23:11 +1000

    removed unused variables

--- caf_solution/add-ons/dynamic_vault_secrets/main.tf
+++ caf_solution/add-ons/dynamic_vault_secrets/main.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.43"
+    }
     azuread = {
       source  = "hashicorp/azuread"
       version = "~> 1.4.0"
