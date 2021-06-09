7ba8bd64b5f228397bbdb4a6eb91ee831484e2e7
Papun Senapati <papun.senapati@servian.com>
2021-06-09 11:03:29 +1000

    removed unwanted providers

--- caf_solution/add-ons/dynamic_vault_secrets/main.tf
+++ caf_solution/add-ons/dynamic_vault_secrets/main.tf
@@ -4,34 +4,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 2.43"
     }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~> 1.4.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 2.2.1"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 2.1.0"
-    }
-    external = {
-      source  = "hashicorp/external"
-      version = "~> 1.2.0"
-    }
-    azuredevops = {
-      source  = "microsoft/azuredevops"
-      version = "~> 0.1.0"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 2.2.0"
-    }
-    azurecaf = {
-      source  = "aztfmod/azurecaf"
-      version = "~> 1.2.0"
-    }
     vault = {
       source  = "hashicorp/vault"
       version = "2.17.0"
