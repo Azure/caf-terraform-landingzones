d0c5c60e7ef817bcb019f4aad1c02852bbaa82da
Arnaud Lheureux <arnaudlh@hotmail.com>
2021-06-18 12:09:37 +0000

    Update branch reference

--- caf_solution/landingzone.tf
+++ caf_solution/landingzone.tf
@@ -1,8 +1,8 @@
 module "solution" {
-  source  = "aztfmod/caf/azurerm"
-  version = "~>5.3.2"
+  # source  = "aztfmod/caf/azurerm"
+  # version = "~>5.3.2"
 
-  # source = "git::https://github.com/aztfmod/terraform-azurerm-caf.git?ref=master"
+  source = "git::https://github.com/aztfmod/terraform-azurerm-caf.git?ref=5.4.0"
   # source = "../../aztfmod"
 
   # azuread                               = var.azuread
