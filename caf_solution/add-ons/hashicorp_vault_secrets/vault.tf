2cda697d8ed433ea09e58791505e133c6aa739d3
Papun Senapati <papun.senapati@servian.com>
2021-06-22 01:06:59 +0000

    added output to debug

--- caf_solution/add-ons/hashicorp_vault_secrets/vault.tf
+++ caf_solution/add-ons/hashicorp_vault_secrets/vault.tf
@@ -9,3 +9,6 @@ module "hashicorp_vault_secrets" {
   disable_read = try(each.value.disable_read, false)
   objects      = local.remote.objects
 }
+output name {
+  value       = local.remote.objects
+}
