831e9c54c87a7751a93927261090bdfbfa793b21
lolorol <lalesle@microsoft.com>
2021-06-24 11:38:49 +0000

    Add variables_object

--- caf_solution/add-ons/azure_devops_v1/azdo_variable_groups.tf
+++ caf_solution/add-ons/azure_devops_v1/azdo_variable_groups.tf
@@ -49,4 +49,13 @@ resource "azuredevops_variable_group" "variable_group" {
     }
   }
 
+  dynamic "variable" {
+    for_each = try(each.value.variables_objects, {})
+
+    content {
+      name  = variable.value.name
+      value = variable.value.value
+    }
+  }
+
 }
