53caf4c8a8143905b7bde3b703dd0c550794f165
Jor Seng <jorseng@gmail.com>
2021-07-08 08:11:47 +0000

    Fix missing resource_key

--- caf_solution/add-ons/hashicorp_vault_secrets/secret_backend_role/vault_azure_secret_backend_role.tf
+++ caf_solution/add-ons/hashicorp_vault_secrets/secret_backend_role/vault_azure_secret_backend_role.tf
@@ -2,7 +2,7 @@ resource "vault_azure_secret_backend_role" "existing_object_id" {
 
   backend               = var.settings.backend
   role                  = var.settings.role
-  application_object_id = var.objects[var.settings.azuread_application.lz_key][var.settings.azuread_application.output_key][var.settings.azuread_application.attribute_key]
+  application_object_id = var.objects[var.settings.azuread_application.lz_key][var.settings.azuread_application.output_key][var.settings.azuread_application.resource_key][var.settings.azuread_application.attribute_key]
   ttl                   = try(var.settings.ttl, null)
   max_ttl               = try(var.settings.max_ttl, null)
 }
