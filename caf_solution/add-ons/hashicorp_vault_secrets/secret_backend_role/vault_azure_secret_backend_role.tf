784db1ccbe1210c915c1570852d818e6ed08bb03
Jor Seng <jorseng@gmail.com>
2021-07-08 08:07:51 +0000

    remove try block

--- caf_solution/add-ons/hashicorp_vault_secrets/secret_backend_role/vault_azure_secret_backend_role.tf
+++ caf_solution/add-ons/hashicorp_vault_secrets/secret_backend_role/vault_azure_secret_backend_role.tf
@@ -2,7 +2,7 @@ resource "vault_azure_secret_backend_role" "existing_object_id" {
 
   backend               = var.settings.backend
   role                  = var.settings.role
-  application_object_id = try(var.objects[var.settings.azuread_application.lz_key][var.settings.azuread_application.output_key][var.settings.azuread_application.attribute_key], null)
-  ttl                   = var.settings.ttl
-  max_ttl               = var.settings.max_ttl
+  application_object_id = var.objects[var.settings.azuread_application.lz_key][var.settings.azuread_application.output_key][var.settings.azuread_application.attribute_key]
+  ttl                   = try(var.settings.ttl, null)
+  max_ttl               = try(var.settings.max_ttl, null)
 }
