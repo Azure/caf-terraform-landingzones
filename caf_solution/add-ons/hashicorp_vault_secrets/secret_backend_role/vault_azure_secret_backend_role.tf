68b0847c678bea9af171b9a013b99381e4c1d846
Jor Seng <jorseng@gmail.com>
2021-07-08 08:53:21 +0000

    test sp object_id

--- caf_solution/add-ons/hashicorp_vault_secrets/secret_backend_role/vault_azure_secret_backend_role.tf
+++ caf_solution/add-ons/hashicorp_vault_secrets/secret_backend_role/vault_azure_secret_backend_role.tf
@@ -2,7 +2,8 @@ resource "vault_azure_secret_backend_role" "existing_object_id" {
 
   backend               = var.settings.backend
   role                  = var.settings.role
-  application_object_id = var.objects[var.settings.azuread_application.lz_key][var.settings.azuread_application.output_key][var.settings.azuread_application.resource_key][var.settings.azuread_application.attribute_key]
+  # application_object_id = var.objects[var.settings.azuread_application.lz_key][var.settings.azuread_application.output_key][var.settings.azuread_application.resource_key][var.settings.azuread_application.attribute_key]
+  application_object_id = "614dffd5-72e8-4c17-949c-fd4a934706d2"
   ttl                   = try(var.settings.ttl, null)
   max_ttl               = try(var.settings.max_ttl, null)
 }
