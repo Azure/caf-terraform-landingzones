5ad41a7a34e48ed4cc81c04c5ea76feab24124c7
Jor Seng <jorseng@gmail.com>
2021-07-09 08:16:04 +0000

    test vault backend role

--- caf_solution/add-ons/hashicorp_vault_secrets/secret_backend_role/vault_azure_secret_backend_role.tf
+++ caf_solution/add-ons/hashicorp_vault_secrets/secret_backend_role/vault_azure_secret_backend_role.tf
@@ -1,6 +1,15 @@
+resource "vault_azure_secret_backend" "azuresp" {
+  path            = var.settings.backend
+  subscription_id = "7e01ba9c-ef41-4252-92e7-5716d7995ad2"
+  tenant_id       = "3217d691-edcd-490d-b5ec-dc724087b782"
+  client_id       = "11111111-2222-3333-4444-333333333333"
+  client_secret   = "1CMihRG0esN2Ow68rMhiyBI8OrHKqkghzvHOFOyJuDZN6JPSY5USTL6mMvWuUlPTeOP18dRayf4EPJp5odCIspiosWMXi1yIuxPM6jhQnbMyXZPbrNIWByQBSIJDeY9oVFA7zxAsO7sWos19Tf4lS59qwFJOdQ7cgOhJQ6a5J5A8DNyajOXIFPbd3Y7VgYUlDfJbnf59JkryvI1nZLQ2Kej2TDJxOBtRMetfEDckqwxYElrbEeIgwny9JX"
+  # environment     = "AzurePublicCloud"
+}
+
 resource "vault_azure_secret_backend_role" "existing_object_id" {
 
-  backend               = var.settings.backend
+  backend               = vault_azure_secret_backend.azuresp.path
   role                  = var.settings.role
   application_object_id = var.objects[var.settings.azuread_application.lz_key][var.settings.azuread_application.output_key][var.settings.azuread_application.resource_key][var.settings.azuread_application.attribute_key]
   ttl                   = try(var.settings.ttl, null)
