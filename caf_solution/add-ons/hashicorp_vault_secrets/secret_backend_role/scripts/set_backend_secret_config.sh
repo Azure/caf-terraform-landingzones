73ddd2e76519af7117f1d55fb93f90b20b0f11a5
Jor Seng <jorseng@gmail.com>
2021-07-13 06:10:21 +0000

    remove temporal script

--- caf_solution/add-ons/hashicorp_vault_secrets/secret_backend_role/scripts/set_backend_secret_config.sh
+++ caf_solution/add-ons/hashicorp_vault_secrets/secret_backend_role/scripts/set_backend_secret_config.sh
@@ -2,7 +2,7 @@
 echo 'Configure vault backend secret config'
 set -e
 # VAULT_ADDR and VAULT_TOKEN is defined in azure pipelines, refactor to handle here.
-vault secrets enable -path=${VAULT_SECRET_BACKEND} azure
+# vault secrets enable -path=${VAULT_SECRET_BACKEND} azure
 
 vault write ${VAULT_SECRET_BACKEND}/config \
   subscription_id=${AZURE_SUBSCRIPTION_ID} \
