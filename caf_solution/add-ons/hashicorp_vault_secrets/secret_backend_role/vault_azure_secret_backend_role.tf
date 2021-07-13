651a459552bf677bd4427bca18a1621df83b7307
Jor Seng <jorseng@gmail.com>
2021-07-13 03:21:16 +0000

    update variable for vault backend

--- caf_solution/add-ons/hashicorp_vault_secrets/secret_backend_role/vault_azure_secret_backend_role.tf
+++ caf_solution/add-ons/hashicorp_vault_secrets/secret_backend_role/vault_azure_secret_backend_role.tf
@@ -1,9 +1,9 @@
 resource "vault_azure_secret_backend" "azure" {
   path            = var.settings.backend
-  subscription_id = "7e01ba9c-ef41-4252-92e7-5716d7995ad2"
-  tenant_id       = "3217d691-edcd-490d-b5ec-dc724087b782"
-  client_id       = "9c355896-bab8-4412-b850-700a9d69ca4c"
-  client_secret   = "1CMihRG0esN2Ow68rMhiyBI8OrHKqkghzvHOFOyJuDZN6JPSY5USTL6mMvWuUlPTeOP18dRayf4EPJp5odCIspiosWMXi1yIuxPM6jhQnbMyXZPbrNIWByQBSIJDeY9oVFA7zxAsO7sWos19Tf4lS59qwFJOdQ7cgOhJQ6a5J5A8DNyajOXIFPbd3Y7VgYUlDfJbnf59JkryvI1nZLQ2Kej2TDJxOBtRMetfEDckqwxYElrbEeIgwny9JX"
+  subscription_id = var.settings.subscription_id
+  tenant_id       = var.settings.tenant_id
+  client_id       = var.settings.client_id
+  client_secret   = var.settings.client_secret
 }
 
 resource "vault_azure_secret_backend_role" "existing_object_id" {
