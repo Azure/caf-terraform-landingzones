2251025886c675097726ab2f15ab5c42d0606074
Nguyen Nhu Hieu <5441003+hieumoscow@users.noreply.github.com>
2021-07-14 13:46:20 +0800

    Update output.tf

--- caf_solution/add-ons/aks_secure_baseline_v2/output.tf
+++ caf_solution/add-ons/aks_secure_baseline_v2/output.tf
@@ -1,7 +1,8 @@
 output "aks_clusters_kubeconfig" {
   value = {
-    aks_kubeconfig_admin_cmd = local.remote.aks_clusters.aks.cluster_re1.aks_kubeconfig_admin_cmd
-    aks_kubeconfig_cmd       = local.remote.aks_clusters.aks.cluster_re1.aks_kubeconfig_cmd
+    aks_kubeconfig_admin_cmd = local.remote.aks_clusters[var.aks_clusters[var.aks_cluster_key].lz_key][var.aks_cluster_key].aks_kubeconfig_admin_cmd
+    aks_kubeconfig_cmd       = local.remote.aks_clusters[var.aks_clusters[var.aks_cluster_key].lz_key][var.aks_cluster_key].aks_kubeconfig_cmd
   }
   sensitive = false
 }
+
