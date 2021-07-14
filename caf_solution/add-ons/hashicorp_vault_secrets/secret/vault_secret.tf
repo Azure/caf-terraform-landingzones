55fea4456d90f001a60b48782b4440eb95d577dd
Jor Seng <jorseng@gmail.com>
2021-07-14 05:34:43 +0000

    fix typo

--- caf_solution/add-ons/hashicorp_vault_secrets/secret/vault_secret.tf
+++ caf_solution/add-ons/hashicorp_vault_secrets/secret/vault_secret.tf
@@ -1,6 +1,7 @@
 locals {
   transposed = {
     for key, value in var.settings.sp_secrets : key => coalesce(
+    # for key, value in var.sp_secrets : key => coalesce(
       try(value.value, null),
       try(var.objects[value.lz_key][value.output_key][value.resource_key][value.attribute_key], null),
       try(var.objects[value.lz_key][value.output_key][value.attribute_key], null),
@@ -42,7 +43,7 @@ resource "null_resource" "set_vault_access" {
 # Service Principal secrets
 data "azurerm_key_vault_secret" "client_secret" {
   for_each = {
-    for key, value in var.settings.sp_secrets : key => value
+    for key, value in var.sp_secrets : key => value
     if try(value.secretname, null) != null && try(value.secretname, null) != ""
   }
   name         = each.value.secretname
--- caf_solution/add-ons/hashicorp_vault_secrets/vault.tf
+++ caf_solution/add-ons/hashicorp_vault_secrets/vault.tf
@@ -2,7 +2,7 @@ module "hashicorp_vault_secrets" {
   source = "./secret"
   for_each = {
     for key, value in var.hashicorp_vault_secrets : key => value
-    if try(value.secrets, null) != null && try(value.secrets, null) != "" && try(value.path, null) != null && try(value.path, null) != ""
+    if try(value.sp_secrets, null) != null && try(value.sp_secrets, null) != "" && try(value.path, null) != null && try(value.path, null) != ""
   }
   objects      = local.remote.objects
   settings     = each.value
