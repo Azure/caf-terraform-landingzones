76e3f889c286e605027b2e6e8cf8f3150ffc61da
Arnaud Lheureux <arnaudlh@hotmail.com>
2021-08-12 05:57:50 +0000

    Update integration branch

--- caf_solution/landingzone.tf
+++ caf_solution/landingzone.tf
@@ -2,7 +2,7 @@ module "solution" {
   # source  = "aztfmod/caf/azurerm"
   # version = "~>5.4.0"
 
-  source = "git::https://github.com/aztfmod/terraform-azurerm-caf.git?ref=ssh_host_rover_support"
+  source = "git::https://github.com/aztfmod/terraform-azurerm-caf.git?ref=patch_5.4.2"
   # source = "../../aztfmod"
 
   azuread                               = local.azuread
