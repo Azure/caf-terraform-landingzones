1c44161c793d9084e02c30677149f9cebbcdb6c2
Jor Seng <jorseng@gmail.com>
2021-10-14 02:32:18 +0000

    add remote object for servicebus namespace

--- caf_solution/local.remote.tf
+++ caf_solution/local.remote.tf
@@ -154,6 +154,9 @@ locals {
     resource_groups = {
       for key, value in try(var.landingzone.tfstates, {}) : key => merge(try(data.terraform_remote_state.remote[key].outputs.objects[key].resource_groups, {}))
     }
+    servicebus_namespaces = {
+      for key, value in try(var.landingzone.tfstates, {}) : key => merge(try(data.terraform_remote_state.remote[key].outputs.objects[key].servicebus_namespaces, {}))
+    }
     storage_accounts = {
       for key, value in try(var.landingzone.tfstates, {}) : key => merge(try(data.terraform_remote_state.remote[key].outputs.objects[key].storage_accounts, {}))
     }
