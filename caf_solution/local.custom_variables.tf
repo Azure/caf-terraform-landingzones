f5f369a7c220d5faed6a909b2c3a614b3fd08478
lolorol <lalesle@microsoft.com>
2021-10-22 13:13:24 +0000

    Improve deep_merge to prevent duplicates

--- caf_solution/local.custom_variables.tf
+++ caf_solution/local.custom_variables.tf
@@ -5,7 +5,22 @@ locals {
   remote_custom_variables = {
     for key, value in try(var.landingzone.tfstates, {}) : "deep_merged_l1" => merge(try(data.terraform_remote_state.remote[key].outputs.custom_variables, {})) ...
   }
-  
+  #
+  # Produces the following structure
+    # + remote_custom_variables      = {
+    #   + deep_merged_l1 = [
+    #       + {},
+    #       + {
+    #           + another_var        = "other"
+    #           + virtual_hub_lz_key = "vhub_prod"
+    #         },
+    #       + {
+    #           + ddos_protection_plan = "/subscription/ddos_plan.id"
+    #           + virtual_hub_lz_key   = "vhub_prod"
+    #         },
+    #     ]
+    # }
+
   deep_merged_l1 = {
     for mapping in
     flatten(
@@ -18,11 +33,45 @@ locals {
           }
         ]
       ]
+    ) : mapping.key => mapping.value ...
+  }
+  #
+  # Produces the folowing structure
+    # + deep_merged_l1               = {
+    #   + another_var          = [
+    #       + "other",
+    #     ]
+    #   + ddos_protection_plan = [
+    #       + "/subscription/ddos_plan.id",
+    #     ]
+    #   + virtual_hub_lz_key   = [
+    #       + "vhub_prod",
+    #       + "vhub_prod",
+    #     ]
+    # }
+
+  deep_merged_l2 = {
+    for mapping in
+    flatten(
+      [
+        for key, value in try(local.deep_merged_l1, {}) :
+        {
+          key = key
+          value = value[0]
+        }
+      ]
     ) : mapping.key => mapping.value
   }
-
+  #
+  # Produces the following structure
+    # + custom_variables             = {
+    #   + another_var          = "other"
+    #   + ddos_protection_plan = "/subscription/ddos_plan.id"
+    #   + virtual_hub_lz_key   = "vhub_prod"
+    # }
+  
   custom_variables = merge(
-    try(local.deep_merged_l1, {}),
+    try(local.deep_merged_l2, {}),
     var.custom_variables
   )
 
