d8cf43a4e67bc6e8499e08f9950d863ccf9c1324
dimitrifc <83534599+dimitrifc@users.noreply.github.com>
2021-10-22 14:24:15 +0200

    added vm custom script extension

--- caf_solution/vm_extensions.tf
+++ caf_solution/vm_extensions.tf
@@ -93,3 +93,21 @@ module "vm_extension_session_host_dscextension" {
   keyvaults          = merge(tomap({ (var.landingzone.key) = module.solution.keyvaults }), try(local.remote.keyvaults, {}))
   wvd_host_pools     = merge(tomap({ (var.landingzone.key) = module.solution.wvd_host_pools }), try(local.remote.wvd_host_pools, {}))
 }
+
+module "vm_extension_custom_scriptextension" {
+  source  = "github.com/volkerwessels/terraform-azurerm-caf//modules/compute/virtual_machine_extensions?ref=5.4.0-modified"
+  #version = "~>5.4.0"
+
+  depends_on = [module.solution]
+
+  for_each = {
+    for key, value in try(var.virtual_machines, {}) : key => value
+    if try(value.virtual_machine_extensions.custom_script, null) != null
+  }
+
+  client_config      = module.solution.client_config
+  virtual_machine_id = module.solution.virtual_machines[each.key].id
+  extension          = each.value.virtual_machine_extensions.custom_script
+  extension_name     = "custom_script"
+  managed_identities = merge(tomap({ (var.landingzone.key) = module.solution.managed_identities }), try(local.remote.managed_identities, {}))
+}
