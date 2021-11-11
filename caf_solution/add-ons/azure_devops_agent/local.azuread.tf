8f043d1cbf7cb4f32b4fcbb0e03ee345e701b2f6
Ricky Chew <41740185+rickychew77@users.noreply.github.com>
2021-11-11 10:07:57 +0800

    Create local.azuread.tf

---
+++ caf_solution/add-ons/azure_devops_agent/local.azuread.tf
@@ -0,0 +1,9 @@
+locals {
+  azuread = merge(
+    var.azuread,
+    {
+      azuread_apps                        = var.azuread_apps
+      azuread_groups                      = var.azuread_groups
+    }
+  )
+}
