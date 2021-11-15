cf38710d5d65a368199ac604fecf0bfa23cc78a8
Paul Bourke <pauldbourke@protonmail.com>
2021-11-15 11:15:09 +0000

    Call file function for helm_release values
    This change allows us to pass a values.yaml file to this module:
    
    helm_charts = {
      traefik = {
        name = "traefik"
        repository = "https://helm.traefik.io/traefik"
        chart      = "traefik"
        namespace  = "kube-system"
        values     = "traefik/values.yaml"
      }
    }
    
    Where traefik/values.yaml is a file alongside your other configuration.

--- caf_solution/add-ons/aks_applications/app/module.tf
+++ caf_solution/add-ons/aks_applications/app/module.tf
@@ -21,7 +21,7 @@ resource "helm_release" "charts" {
   timeout          = try(each.value.timeout, 900)
   skip_crds        = try(each.value.skip_crds, false)
   create_namespace = try(each.value.create_namespace, false)
-  values           = try(each.value.values, null)
+  values           = try(file(each.value.values), null)
 
   dynamic "set" {
     for_each = try(each.value.sets, {})
@@ -41,7 +41,4 @@ resource "helm_release" "charts" {
 
 
   # depends_on = [kubernetes_namespace.namespaces]
-  #   values = [
-  #     "${file("values.yaml")}"
-  #   ]
 }
