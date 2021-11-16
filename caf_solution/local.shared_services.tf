47e7fc486ca2ef4bb4b4abc918c3c9175c1913e2
hyperfocus1337 <hyperfocus@hotmail.com>
2021-11-16 16:37:39 +0100

    Add monitor_autoscale_setting(s) module

--- caf_solution/local.shared_services.tf
+++ caf_solution/local.shared_services.tf
@@ -2,13 +2,14 @@ locals {
   shared_services = merge(
     var.shared_services,
     {
-      automations              = var.automations
-      recovery_vaults          = var.recovery_vaults
-      monitoring               = var.monitoring
-      shared_image_galleries   = var.shared_image_galleries
-      image_definitions        = var.image_definitions
-      packer_service_principal = var.packer_service_principal
-      packer_managed_identity  = var.packer_managed_identity
+      automations                = var.automations
+      recovery_vaults            = var.recovery_vaults
+      monitoring                 = var.monitoring
+      shared_image_galleries     = var.shared_image_galleries
+      monitor_autoscale_settings = var.monitor_autoscale_settings
+      image_definitions          = var.image_definitions
+      packer_service_principal   = var.packer_service_principal
+      packer_managed_identity    = var.packer_managed_identity
     }
   )
 }
