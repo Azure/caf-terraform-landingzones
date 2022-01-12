0ae994d6ee64f2261f94cc76ba58d06675035031
Jor Seng <jorseng@gmail.com>
2022-01-12 06:39:27 +0000

    update source

--- caf_solution/landingzone.tf
+++ caf_solution/landingzone.tf
@@ -1,9 +1,9 @@
 module "solution" {
-  # source  = "aztfmod/caf/azurerm"
-  # version = "~>5.4.2"
+  source  = "aztfmod/caf/azurerm"
+  version = "~>5.4.2"
 
   # source = "git::https://github.com/aztfmod/terraform-azurerm-caf.git?ref=patch.5.5.0"
-  source = "../../aztfmod"
+  # source = "../../aztfmod"
 
   providers = {
     azurerm.vhub = azurerm.vhub
