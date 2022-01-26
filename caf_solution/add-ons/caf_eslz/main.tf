5006171b929aca28220321398638043859ee88c3
LaurentLesle <lalesle@microsoft.com>
2022-01-26 03:30:15 +0000

    Fix ESLZ plugin to add new MG while using the ASVM

--- caf_solution/add-ons/caf_eslz/main.tf
+++ caf_solution/add-ons/caf_eslz/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.80.0"
+      version = "~> 2.93.1"
     }
   }
   required_version = ">= 0.14"
