821b1054fb9a4030b58292f2998d38ad70ec97e8
hyperfocus1337 <hyperfocus@hotmail.com>
2022-02-09 11:44:51 +0100

    Add application health extension

--- caf_solution/vmss_extensions.tf
+++ caf_solution/vmss_extensions.tf
@@ -39,3 +39,21 @@ module "vmss_extension_custom_scriptextension" {
   managed_identities           = merge(tomap({ (var.landingzone.key) = module.solution.managed_identities }), try(local.remote.managed_identities, {}))
   storage_accounts             = merge(tomap({ (var.landingzone.key) = module.solution.storage_accounts }), try(local.remote.storage_accounts, {}))
 }
+
+module "vmss_extension_application_health_extension" {
+  source  = "aztfmod/caf/azurerm//modules/compute/virtual_machine_scale_set_extensions"
+  version = "5.5.1"
+
+  # source = "git::https://github.com/aztfmod/terraform-azurerm-caf.git//modules/compute/virtual_machine_scale_set_extensions?ref=main"
+
+  depends_on = [module.solution]
+
+  for_each = {
+    for key, value in try(var.virtual_machine_scale_sets, {}) : key => value
+    if try(value.virtual_machine_scale_set_extensions.microsoft_azure_health_extension, null) != null
+  }
+
+  virtual_machine_scale_set_id = module.solution.virtual_machine_scale_sets[each.key].id
+  extension                    = each.value.virtual_machine_scale_set_extensions.microsoft_azure_health_extension
+  extension_name               = "microsoft_azure_health_extension"
+}
