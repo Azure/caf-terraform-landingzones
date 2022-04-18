5bcecf851865a68a1684c0204a80503a3d6ab56d
LaurentLesle <lalesle@microsoft.com>
2022-04-18 06:47:36 +0000

    Update for aztfmod 5.5.7

--- caf_solution/landingzone.tf
+++ caf_solution/landingzone.tf
@@ -3,7 +3,7 @@ module "solution" {
 #   version = "5.5.5"
 
   # during dev cycles for the module, you can pick dev branches from GitHub, or from a local fork
-  source = "git::https://github.com/aztfmod/terraform-azurerm-caf.git?ref=AL-2203-nets"
+  source = "git::https://github.com/aztfmod/terraform-azurerm-caf.git?ref=main"
   # source = "../../aztfmod"
 
   providers = {
