ede2968ad55c0685377d6361e1f7ff32a3e9cf0c
lolorol <LaurentLesle@users.noreply.github.com>
2022-05-10 06:51:32 +0000

    Fix in templates

--- templates/platform/deploy_platform.sh
+++ templates/platform/deploy_platform.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-export ANSIBLE_DISPLAY_SKIPPED_HOSTS=False
+
+export ANSIBLE_DISPLAY_SKIPPED_HOSTS=False 
 
 ansible-playbook /tf/caf/landingzones/templates/ansible/walk-through-single.yaml \
   -e topology_file=/tf/caf/landingzones/templates/platform/single_subscription.yaml \
@@ -9,4 +10,5 @@ ansible-playbook /tf/caf/landingzones/templates/ansible/walk-through-single.yaml
   -e platform_configuration_folder=/tf/caf/configuration \
   -e platform_definition_folder=/tf/caf/platform/definition \
   -e platform_template_folder=/tf/caf/platform/template \
+  -e caf_landingzone_branch='2204.1.int' \
   --extra-vars "@/tf/caf/landingzones/templates/platform/template_topology.yaml"
