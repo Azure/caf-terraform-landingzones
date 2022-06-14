4979f4f767111fc29d0c8ca47554a72834a72d13
dimitrifc <>
2022-06-14 13:43:53 +0200

    version 2.96

--- caf_solution/add-ons/caf_eslz/main.tf
+++ caf_solution/add-ons/caf_eslz/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.93.1"
+      version = "~> 2.96"
     }
   }
   required_version = ">= 0.14"
