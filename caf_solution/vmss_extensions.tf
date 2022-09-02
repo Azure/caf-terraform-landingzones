9b3caf326f078d039b1465806599de0e6b6325ec
hyperfocus1337 <hyperfocus@hotmail.com>
2022-09-02 10:44:22 +0200

    Comment vmss_extension_custom_scriptextension module

--- caf_solution/vmss_extensions.tf
+++ caf_solution/vmss_extensions.tf
@@ -19,27 +19,27 @@ module "vmss_extension_microsoft_azure_domainjoin" {
 }
 
 
-module "vmss_extension_custom_scriptextension" {
-  # source  = "aztfmod/caf/azurerm//modules/compute/virtual_machine_scale_set_extensions"
-  # version = "5.5.5"
+# module "vmss_extension_custom_scriptextension" {
+#   # source  = "aztfmod/caf/azurerm//modules/compute/virtual_machine_scale_set_extensions"
+#   # version = "5.5.5"
 
-  source = "git::https://github.com/VolkerWessels/terraform-azurerm-caf.git//modules/compute/virtual_machine_scale_set_extensions?ref=vw-combined"
+#   source = "git::https://github.com/VolkerWessels/terraform-azurerm-caf.git//modules/compute/virtual_machine_scale_set_extensions?ref=vw-combined"
 
-  depends_on = [module.solution]
+#   depends_on = [module.solution]
 
-  for_each = {
-    for key, value in try(var.virtual_machine_scale_sets, {}) : key => value
-    if try(value.virtual_machine_scale_set_extensions.custom_script, null) != null
-  }
+#   for_each = {
+#     for key, value in try(var.virtual_machine_scale_sets, {}) : key => value
+#     if try(value.virtual_machine_scale_set_extensions.custom_script, null) != null
+#   }
 
-  client_config                     = module.solution.client_config
-  virtual_machine_scale_set_id      = module.solution.virtual_machine_scale_sets[each.key].id
-  virtual_machine_scale_set_os_type = module.solution.virtual_machine_scale_sets[each.key].os_type
-  extension                         = each.value.virtual_machine_scale_set_extensions.custom_script
-  extension_name                    = "custom_script"
-  managed_identities                = merge(tomap({ (var.landingzone.key) = module.solution.managed_identities }), try(local.remote.managed_identities, {}))
-  storage_accounts                  = merge(tomap({ (var.landingzone.key) = module.solution.storage_accounts }), try(local.remote.storage_accounts, {}))
-}
+#   client_config                     = module.solution.client_config
+#   virtual_machine_scale_set_id      = module.solution.virtual_machine_scale_sets[each.key].id
+#   virtual_machine_scale_set_os_type = module.solution.virtual_machine_scale_sets[each.key].os_type
+#   extension                         = each.value.virtual_machine_scale_set_extensions.custom_script
+#   extension_name                    = "custom_script"
+#   managed_identities                = merge(tomap({ (var.landingzone.key) = module.solution.managed_identities }), try(local.remote.managed_identities, {}))
+#   storage_accounts                  = merge(tomap({ (var.landingzone.key) = module.solution.storage_accounts }), try(local.remote.storage_accounts, {}))
+# }
 
 module "vmss_extension_keyvault_extension" {
   # source  = "aztfmod/caf/azurerm//modules/compute/virtual_machine_scale_set_extensions"
