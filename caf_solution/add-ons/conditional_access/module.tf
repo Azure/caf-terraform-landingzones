0f981fe40e6d0946ab0cc8268372afc9df42c614
Jelle van de Haterd <JvdHaterd@volkerwessels.com>
2022-10-13 15:56:24 +0200

    module update

--- caf_solution/add-ons/conditional_access/module.tf
+++ caf_solution/add-ons/conditional_access/module.tf
@@ -1,5 +1,6 @@
 module "conditional_access" {
   source = "git::https://github.com/VolkerWessels/terraform-conditional-access.git?ref=main"
 
-
+  library_path = var.library_path
+  template_file_variables = var.template_file_variables
 }
