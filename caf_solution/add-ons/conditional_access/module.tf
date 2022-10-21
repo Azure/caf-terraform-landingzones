50be7d12649017d4093f7411a412a87308571f82
Jelle van de Haterd <JvdHaterd@volkerwessels.com>
2022-10-21 15:30:40 +0200

    Named Locations

--- caf_solution/add-ons/conditional_access/module.tf
+++ caf_solution/add-ons/conditional_access/module.tf
@@ -1,7 +1,8 @@
 module "conditional_access" {
 #  depends_on = [module.caf]
-  source = "git::https://github.com/VolkerWessels/terraform-conditional-access.git?ref=main"
+  source = "git::https://github.com/VolkerWessels/terraform-conditional-access.git?ref=feature/named_locations"
 
   library_path = var.library_path
   template_file_variables = var.template_file_variables
+  named_locations = var.named_locations
 }
