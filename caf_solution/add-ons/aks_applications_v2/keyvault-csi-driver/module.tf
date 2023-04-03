7d66bfb232b60cb1d9480cd5fc127c4febd501e3
Najeeb Vayal Valappil <najeeb.vayal-valappil@adesso.de>
2023-04-03 14:59:57 +0200

    kv object id reference

--- caf_solution/add-ons/aks_applications_v2/keyvault-csi-driver/module.tf
+++ caf_solution/add-ons/aks_applications_v2/keyvault-csi-driver/module.tf
@@ -1,7 +1,7 @@
 resource "azurerm_role_assignment" "for" {
   for_each = var.settings
 
-  principal_id         = try(each.value.aks_clusters.uuid, var.aks_clusters[each.value.aks_cluster[lz_key]][each.value.aks_cluster[key]].addon_profile[0].azure_keyvault_secrets_provider.secret_identity.client_id)
+  principal_id         = try(each.value.aks_clusters.uuid, var.aks_clusters[each.value.aks_cluster[lz_key]][each.value.aks_cluster[key]].addon_profile[0].azure_keyvault_secrets_provider.secret_identity.object_id)
   role_definition_name = each.value.role_definition_name
   scope                = each.value.keyvault[lz_key] == null ? each.value.keyvault[keyvault_id] : var.keyvaults[ each.value.keyvault[lz_key][keyvault_key].id]
 }
