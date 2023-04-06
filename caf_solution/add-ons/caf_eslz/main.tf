b732cebb975d3e34b46ca72fa32e686618c01d25
lolorol <LaurentLesle@users.noreply.github.com>
2023-04-06 06:53:04 +0000

    Update to default to the loggedin sub for mono sub deployment

--- caf_solution/add-ons/caf_eslz/main.tf
+++ caf_solution/add-ons/caf_eslz/main.tf
@@ -19,7 +19,7 @@ provider "azurerm" {
   partner_id = "ca4078f8-9bc4-471b-ab5b-3af6b86a42c8"
   alias      = "connectivity"
   features {}
-  subscription_id = var.subscription_id_connectivity == null ? data.azurerm_client_config.subscription_id : var.subscription_id_connectivity
+  subscription_id = var.subscription_id_connectivity == null ? data.azurerm_client_config.current.subscription_id : var.subscription_id_connectivity
   tenant_id       = var.tenant_id
 }
 
@@ -27,7 +27,7 @@ provider "azurerm" {
   partner_id = "ca4078f8-9bc4-471b-ab5b-3af6b86a42c8"
   alias      = "management"
   features {}
-  subscription_id = var.subscription_id_management == null ? data.azurerm_client_config.subscription_id : var.subscription_id_management == null
+  subscription_id = var.subscription_id_management == null ? data.azurerm_client_config.current.subscription_id : var.subscription_id_management == null
   tenant_id       = var.tenant_id
 }
 
