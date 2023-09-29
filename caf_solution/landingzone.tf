1cadc1a4251d687b05578af0e52d15d693acae3e
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-09-29 04:16:31 +0000

    Update module

--- caf_solution/landingzone.tf
+++ caf_solution/landingzone.tf
@@ -1,6 +1,6 @@
 module "solution" {
   source  = "aztfmod/caf/azurerm"
-  version = "5.7.4"
+  version = "5.7.5"
   # source = "git::https://github.com/aztfmod/terraform-azurerm-caf.git?ref=main"
 
   providers = {
