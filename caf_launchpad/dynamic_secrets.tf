2034400f8ae442b0ae24b51444886dcfab898cd8
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-11-24 07:22:33 +0000

    Update components

--- caf_launchpad/dynamic_secrets.tf
+++ caf_launchpad/dynamic_secrets.tf
@@ -1,7 +1,7 @@
 
 module "dynamic_keyvault_secrets" {
   source  = "aztfmod/caf/azurerm//modules/security/dynamic_keyvault_secrets"
-  version = "5.7.6"
+  version = "5.7.7"
 
   for_each = try(var.dynamic_keyvault_secrets, {})
 
