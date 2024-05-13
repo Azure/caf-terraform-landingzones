0375bdd799f02629577744a554f502d665c7ecb1
chian <91816369+chianw@users.noreply.github.com>
2024-05-13 03:27:49 +0000

    update aztfmod to 5.7.11

--- caf_solution/dynamic_secrets.tf
+++ caf_solution/dynamic_secrets.tf
@@ -1,6 +1,6 @@
 module "dynamic_keyvault_secrets" {
   source  = "aztfmod/caf/azurerm//modules/security/dynamic_keyvault_secrets"
-  version = "5.7.10"
+  version = "5.7.11"
 
   for_each = {
     for keyvault_key, secrets in try(var.dynamic_keyvault_secrets, {}) : keyvault_key => {
